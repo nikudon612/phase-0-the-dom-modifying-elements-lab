a7e5be73945127b0bb0d02d186a138a015ce900c
Nick Bechtel <bechtel.nick@gmail.com>
2021-09-30 18:48:15 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+var testOne = document.getElementById("main");
+testOne.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "<p>YOUR-NAME is the champion</p>";
+document.head.append(newHeader);
